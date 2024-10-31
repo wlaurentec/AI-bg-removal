b7270cdae88d3d29064733aa82347b257208ba58
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-10-30 22:45:26 -0500

    add Steps.jsx component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header'
+import Steps from '../components/Steps'
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Steps />
     </div>
   )
 }
