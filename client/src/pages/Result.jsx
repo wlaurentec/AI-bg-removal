16a7842a08b6ba8f8fc95964cc8296011f36f545
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-01 09:00:19 -0500

    add result

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,10 +1,24 @@
-
+import { assets } from "../assets/assets";
 const Result = () => {
   return (
-    <div>
-      Result
+    <div className="bg-cyan-400 mx-4 my-3 lg:mx-44 mt-14 min-h-[75vh]">
+      <div className="bg-slate-200">
+        {/* Image Container */}
+        <div>
+          {/* Left Side */}
+          <div>
+            <p>Original</p>
+            <img src={assets.image_w_bg} alt="" />
+          </div>
+          {/* Right Side */}
+          <div>
+            <p>Background Removed</p>
+            <img src={assets.image_wo_bg} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
