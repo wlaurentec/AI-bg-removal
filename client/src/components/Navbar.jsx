6d1cce22c6b2ac83b79cab28369286fdf24dfb77
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-04 17:14:44 -0500

    add Credits in Fronted

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,42 @@
 import { Link } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { useContext, useEffect } from "react";
+import { AppContext } from "../context/AppContext";
 const Navbar = () => {
   const { openSignIn } = useClerk();
-  const { isSignedIn, user} = useUser();
+  const { isSignedIn, user } = useUser();
+  const { credit, loadCreditsData } = useContext(AppContext);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      loadCreditsData();
+    }
+  }, [isSignedIn]);
 
   return (
     <div className="flex items-center justify-between mx-4 py-3 lg:mx-44 ">
-      <Link to="/"><img className="w-32 sm:w-44" src={assets.logo} alt="" /></Link>
-      {isSignedIn
-      ? <div>
-        <UserButton />
-      </div>
-      : <button onClick={() => openSignIn({})} className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full">
-        Get started <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
-      </button>}
+      <Link to="/">
+        <img className="w-32 sm:w-44" src={assets.logo} alt="" />
+      </Link>
+      {isSignedIn ? (
+        <div className="flex items-center gap-2 sm:gap-3">
+          <button className="flex items-center gap-2 bg-blue-100 px-4 sm:px-7 py-1.5 sm:py-2.5 rounded-full hover:scale-105 transition-all duration-700">
+            <img className="w-5" src={assets.credit_icon} alt="" />
+            <p className="text-xs sm:text-sm font-medium text-gray-600">Credits: {credit}</p>
+          </button>
+          <p className="text-gray-600 max-sm:hidden">Hi, {user.fullName}</p>
+          <UserButton />
+        </div>
+      ) : (
+        <button
+          onClick={() => openSignIn({})}
+          className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full"
+        >
+          Get started{" "}
+          <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="" />
+        </button>
+      )}
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import AppContextProvider from './context/AppContext.jsx'
 
 
 // Import your publishable key
@@ -15,8 +16,10 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </ClerkProvider>
   </BrowserRouter>,
 )
