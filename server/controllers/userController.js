ba56715420a2885c295ae0ebcec508c566c2874b
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-04 09:22:15 -0500

    add userController

---
+++ server/controllers/userController.js
@@ -0,0 +1,7 @@
+
+
+// API Controller Function to Manage Clerk User with database
+// http://localhost:4000/api/user/webhooks
+const clerkWebhooks = async (req, res) => {
+  
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
 
 // App Config
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+await connectDB();
 
 // Initialize middlewares
 app.use(express.json());
