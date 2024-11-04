911c6bfdf2a965de755ccd4ef675a99237686dc5
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-04 12:48:40 -0500

    add userCredits controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -57,4 +57,17 @@ const clerkWebhooks = async (req, res) => {
   }
 };
 
-export { clerkWebhooks };
+// API Controller function to get user available credits data
+const userCredtis = async (req, res) => {
+  try {
+    const { clerkId } = req.body;
+
+    const userData = await userModel.findOne({ clerkId });
+    res.json({ success: true, credits: userData.creditBalance });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { clerkWebhooks, userCredtis };
