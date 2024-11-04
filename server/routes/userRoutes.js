65b553f589de9daf0242b2301fc62fa3b96d65e7
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-11-04 14:42:02 -0500

    add authUser.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { clerkWebhooks } from "../controllers/userController.js";
+import authUser from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhooks);
+userRouter.get("/credits", authUser, userCredtis);
 
 export default userRouter;
